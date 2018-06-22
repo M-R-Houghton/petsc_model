e518bce79350058192dc10f6b1f3613f6c19e66f
M-R-Houghton <sparky3@live.co.uk>
2018-06-22 12:34:08 +0100

    added function prototypes

--- include/localAssemblyBend.h
+++ include/localAssemblyBend.h
@@ -3,7 +3,52 @@
 
 #include "localAssemblyStretch.h"
 
+/**
+ * \brief Calculates the bending term kappa
+ * \param box_ptr
+ * \param par_ptr
+ * \param fIndex
+ * \return
+ */
+PetscScalar calculateKappa(Box *box_ptr, Parameters *par_ptr, PetscInt fIndex);
 
+/**
+ * \brief Adds local bend information for a single fibre to global system
+ * Loops through a fibre in triplets
+ * \param box_ptr
+ * \param par_ptr
+ * \param globalMat_H
+ * \param globalVec_B
+ * \param fIndex
+ * \return
+ */
+PetscInt addFibreLocalBend(Box *box_ptr, Parameters *par_ptr, Mat globalMat_H, Vec globalVec_B, PetscInt fIndex);
+
+/**
+ * \brief Assembles the local bend matrix of a given triplet
+ * \param s_alphaOmega_ptr
+ * \param s_omegaBeta_ptr
+ * \param s_alphaBeta_ptr
+ * \param bConst
+ * \param localBendMat_A
+ */
+PetscErrorCode makeBendMatrix(PetscScalar *s_alphaOmega_ptr, PetscScalar *s_omegaBeta_ptr, PetscScalar *s_alphaBeta_ptr, 
+								PetscScalar bConst, Mat localBendMat_A);
+
+/**
+ * \brief Assembles the local bend RHS vector of a given triplet
+ * \param alpha_ptr
+ * \param omega_ptr
+ * \param beta_ptr
+ * \param s_alphaOmega_ptr
+ * \param s_omegaBeta_ptr
+ * \param s_alphaBeta_ptr
+ * \param bConst
+ * \param localBendVec_b
+ */
+PetscErrorCode makeBendVec(Node *alpha_ptr, Node *omega_ptr, Node *beta_ptr,
+                    PetscScalar *s_alphaOmega_ptr, PetscScalar *s_omegaBeta_ptr,
+                    PetscScalar *s_alphaBeta_ptr, PetscScalar bConst, Vec *localBendVec_b);
 
 
 #endif
--- include/localAssemblyStretch.h
+++ include/localAssemblyStretch.h
@@ -3,7 +3,45 @@
 
 #include "networkRead.h"
 
+/**
+ * \brief Calculates the stretching constant k
+ * \param box_ptr
+ * \param par_ptr
+ * \param fIndex
+ * \param segLength
+ * \return
+ */
+PetscScalar calculateK(Box *box_ptr, Parameters *par_ptr, PetscInt fIndex, PetscScalar segLength);
 
+/**
+ * \brief Adds local stretch information for a single fibre to global system
+ * Loops through a fibre in pairs
+ * \param box_ptr
+ * \param par_ptr
+ * \param globalMat_H
+ * \param globalVec_B
+ * \param fIndex
+ * \return
+ */
+PetscErrorCode addFibreLocalStretch(Box *box_ptr, Parameters *par_ptr, Mat globalMat_H, Vec globalVec_B, PetscInt fIndex);
+
+/**
+ * \brief Assembles the local stretch matrix of a given pair
+ * \param k
+ * \param tangVec_ptr
+ * \param localStretchMat_A
+ */
+PetscErrorCode makeStretchMatrix(PetscScalar k, PetscScalar *tangVec_ptr, Mat localStretchMat_A);
+
+/**
+ * \brief Assembles the local stretch RHS vector of a given pair
+ * \param alpha_ptr
+ * \param beta_ptr
+ * \param k
+ * \param tangVec_ptr
+ * \param localStretchVec_b
+ */
+PetscErrorCode makeStretchVec(Node *alpha_ptr, Node *beta_ptr, PetscScalar k, PetscScalar *tangVec_ptr, Vec localStretchVec_b);
 
 
 #endif
