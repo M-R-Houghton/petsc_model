b8a363a8c9bae5b858f865a9efbff470ae36292f
M-R-Houghton <sparky3@live.co.uk>
2018-06-22 17:51:06 +0100

    got file comparisons working but not reliable

--- tests/unitTests/src/networkWriteUnitTests.c
+++ tests/unitTests/src/networkWriteUnitTests.c
@@ -1,11 +1,20 @@
 #include <petscksp.h>
 #include <criterion/criterion.h>
+#include <criterion/redirect.h>
 #include "networkWrite.h"
 
 Test(testNetworkWrite, testOutputFile)
 {
-	const char *tmpFile = "tmpOutFile.dat";
+	const char *tmpFile = "tmp1OutFile.dat";
 	Box *box_ptr = makeBox(1,2,3,4,5,1,1,1);
+
+	FILE *file1_ptr = fopen(tmpFile, "w+");
+	FILE *file2_ptr = fopen(tmpFile, "w+");
+
+	cr_expect_file_contents_neq(file1_ptr, file2_ptr);
+
+	fclose(file1_ptr);
+	fclose(file2_ptr);
 
 	//cr_expect(networkWrite(tmpFile,box_ptr) == 0);
 }
@@ -19,7 +28,13 @@ Test(testWriteBoxLine, testOutputValues)
 
 	writeBoxLine(file_ptr, box_ptr);
 
+	const char *box_str = "b 1 2 3.000000 4.000000 5.000000 1 1 1";
+	
+	//cr_expect_file_contents_eq_str(file_ptr, box_str);
+	cr_expect_file_contents_eq(file_ptr, file_ptr);
+
 	fclose(file_ptr);
+
 	destroyBox(box_ptr);
 }
 
