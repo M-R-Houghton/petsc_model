7864c40f61700f2fb97024fb49058fedbdd2a27e
M-R-Houghton <sparky3@live.co.uk>
2018-09-27 17:38:23 +0100

    also upload shell scripts

---
+++ quickRun2D.sh
@@ -0,0 +1,21 @@
+mpirun -n 1 ./model data/par/lmb_2d_01.par
+mpirun -n 1 ./model data/par/lmb_2d_02.par
+mpirun -n 1 ./model data/par/lmb_2d_add_01.par
+mpirun -n 1 ./model data/par/lmb_2d_add_02.par
+mpirun -n 1 ./model data/par/lmb_2d_add_03.par
+mpirun -n 1 ./model data/par/lmb_2d_add_04.par
+mpirun -n 1 ./model data/par/lmb_2d_lshft.par
+mpirun -n 1 ./model data/par/lmb_2d_rshft.par
+
+mpirun -n 1 ./model data/par/dil_0_94.par
+
+mpirun -n 1 ./model data/par/rnd_2d_01.par
+mpirun -n 1 ./model data/par/rnd_2d_02.par
+mpirun -n 1 ./model data/par/rnd_2d_03.par
+mpirun -n 1 ./model data/par/rnd_2d_04.par
+mpirun -n 1 ./model data/par/rnd_2d_05.par
+mpirun -n 1 ./model data/par/rnd_2d_06.par
+mpirun -n 1 ./model data/par/rnd_2d_07.par
+mpirun -n 1 ./model data/par/rnd_2d_08.par
+mpirun -n 1 ./model data/par/rnd_2d_09.par
+mpirun -n 1 ./model data/par/rnd_2d_10.par
---
+++ quickRun3D.sh
@@ -0,0 +1,16 @@
+mpirun -n 1 ./model data/par/tri_3d_01.par
+mpirun -n 1 ./model data/par/tri_3d_02.par
+mpirun -n 1 ./model data/par/tri_3d_03.par
+mpirun -n 1 ./model data/par/tri_3d_04.par
+
+mpirun -n 1 ./model data/par/tri_3d_add_01.par
+mpirun -n 1 ./model data/par/tri_3d_add_02.par
+mpirun -n 1 ./model data/par/tri_3d_add_03.par
+mpirun -n 1 ./model data/par/tri_3d_add_04.par
+
+mpirun -n 1 ./model data/par/lat_3d_02.par
+
+#mpirun -n 1 ./model data/par/rnd_3d_s01_f600.par
+#mpirun -n 1 ./model data/par/rnd_3d_s01_f600_nt.par
+#mpirun -n 1 ./model data/par/rnd_3d_s01_f800.par
+#mpirun -n 1 ./model data/par/rnd_3d_s01_f800_nt.par
