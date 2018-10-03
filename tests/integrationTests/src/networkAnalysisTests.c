8bbdb16ba629705c88c4915423615716171520c3
M-R-Houghton <sparky3@live.co.uk>
2018-10-03 10:57:44 +0100

    forgot to commit changes to integration tests

--- tests/integrationTests/src/networkAnalysisTests.c
+++ tests/integrationTests/src/networkAnalysisTests.c
@@ -3,8 +3,8 @@
 /* Tests network analysis routine */
 PetscErrorCode test_networkAnalysis()
 {
-	PetscErrorCode ierr;
-	ierr = networkAnalysis();CHKERRQ(ierr);
+	PetscErrorCode ierr = 0;
+	//ierr = networkAnalysis(box_ptr, par_ptr);CHKERRQ(ierr);
 	
 	return ierr;
 }
