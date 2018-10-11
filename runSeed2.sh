50e6cf0aae5850a22eba96f8cbf25b1b142d0821
M-R-Houghton <sparky3@live.co.uk>
2018-10-11 14:55:08 +0100

    add scripts to put on arc3

---
+++ runSeed2.sh
@@ -0,0 +1,17 @@
+# Export current environment
+#$ -cwd -V
+# Set a 10 min limit
+#$ -l h_rt=6:00:00
+# Set memory required
+#$ -l h_vmem=16G
+# Send email at start and end
+#$ -m be
+# run petsc using command file
+#make clean && make
+
+#mpiexec -n 1 ./model data/par/rnd3D/rnd_3d_s02_f600.par
+mpiexec -n 1 ./model data/par/rnd3D/rnd_3d_s02_f600.par -ksp_type gmres -ksp_gmres_restart 100 -pc_type jacobi 
+#mpiexec -n 1 ./model data/par/rnd3D/rnd_3d_s02_f800.par
+mpiexec -n 1 ./model data/par/rnd3D/rnd_3d_s02_f800.par -ksp_type gmres -ksp_gmres_restart 100 -pc_type jacobi 
+#mpiexec -n 1 ./model data/par/rnd3D/rnd_3d_s02_f1000.par
+mpiexec -n 1 ./model data/par/rnd3D/rnd_3d_s02_f1000.par -ksp_type gmres -ksp_gmres_restart 100 -pc_type jacobi 
---
+++ runSmallCases.sh
@@ -0,0 +1,13 @@
+# Export current environment
+#$ -cwd -V
+# Set a 10 min limit
+#$ -l h_rt=1:00:00
+# Set memory required
+#$ -l h_vmem=16G
+# Send email at start and end
+#$ -m be
+# run petsc using command file
+#cp data/sol/sol.f3tRnd_s01_f800_t5_jcb
+make clean && make
+#mpiexec -n 1 ./model -ksp_type gmres -pc_type bjacobi -pc_bjacobi_blocks 9 -sub_ksp_type preonly -sub_pc_type lu
+mpiexec -n 1 ./model data/par/tri/tri_3d_01.par -log_view
