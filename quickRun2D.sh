e35e4b1fc4595d2a6c429691699014d303fe1abe
M-R-Houghton <sparky3@live.co.uk>
2018-11-06 13:03:23 +0000

    updated missing lambda values - note gamma value when testing

--- quickRun2D.sh
+++ quickRun2D.sh
@@ -1,21 +1,88 @@
-mpirun -n 1 ./model data/par/lmb_2d_01.par
-mpirun -n 1 ./model data/par/lmb_2d_02.par
-mpirun -n 1 ./model data/par/lmb_2d_add_01.par
-mpirun -n 1 ./model data/par/lmb_2d_add_02.par
-mpirun -n 1 ./model data/par/lmb_2d_add_03.par
-mpirun -n 1 ./model data/par/lmb_2d_add_04.par
-mpirun -n 1 ./model data/par/lmb_2d_lshft.par
-mpirun -n 1 ./model data/par/lmb_2d_rshft.par
+mpirun -n 1 ./model data/par/lmb/lmb_2d_01.par
+1e-16 mpirun -n 1 ./model data/par/lmb/lmb_2d_02.par
+1e-16 mpirun -n 1 ./model data/par/lmb/lmb_2d_add_01.par
+1e-16 mpirun -n 1 ./model data/par/lmb/lmb_2d_add_02.par
+1e-16 mpirun -n 1 ./model data/par/lmb/lmb_2d_add_03.par
+1e-16 mpirun -n 1 ./model data/par/lmb/lmb_2d_add_04.par
+1e-16 mpirun -n 1 ./model data/par/lmb/lmb_2d_lshft.par
+1e-16 mpirun -n 1 ./model data/par/lmb/lmb_2d_rshft.par
+1e-16 
+mpirun -n 1 ./model data/par/lat2D/dil_0_94.par
+1e-16 
+#mpirun -n 1 ./model data/par/lat2D/dil_0_94.par 1e-16 -ksp_type gmres -pc_type none -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/lat2D/dil_0_94_gm_npc.ppm
+#mpirun -n 1 ./model data/par/lat2D/dil_0_94.par 1e-16 -ksp_type gmres -pc_type jacobi -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/lat2D/dil_0_94_gm_wjc.ppm
+#mpirun -n 1 ./model data/par/lat2D/dil_0_94.par 1e-16 -ksp_type gmres -pc_type sor -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/lat2D/dil_0_94_gm_sor.ppm
+#mpirun -n 1 ./model data/par/lat2D/dil_0_94.par 1e-16 -ksp_type gmres -pc_type eisenstat -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/lat2D/dil_0_94_gm_eis.ppm
+#mpirun -n 1 ./model data/par/lat2D/dil_0_94.par 1e-16 -ksp_type gmres -pc_type ilu -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/lat2D/dil_0_94_gm_ilu.ppm
 
-mpirun -n 1 ./model data/par/dil_0_94.par
+mpirun -n 1 ./model data/par/rnd2D/rnd_2d_01.par
+1e-16 mpirun -n 1 ./model data/par/rnd2D/rnd_2d_02.par
+1e-16 mpirun -n 1 ./model data/par/rnd2D/rnd_2d_03.par
+1e-16 #mpirun -n 1 ./model data/par/rnd2D/rnd_2d_04.par
+1e-16 #mpirun -n 1 ./model data/par/rnd2D/rnd_2d_05.par
+1e-16 #mpirun -n 1 ./model data/par/rnd2D/rnd_2d_06.par
+1e-16 #mpirun -n 1 ./model data/par/rnd2D/rnd_2d_07.par
+1e-16 #mpirun -n 1 ./model data/par/rnd2D/rnd_2d_08.par
+1e-16 #mpirun -n 1 ./model data/par/rnd2D/rnd_2d_09.par
+1e-16 #mpirun -n 1 ./model data/par/rnd2D/rnd_2d_10.par
+1e-16 
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_01.par 1e-16 -ksp_type gmres -pc_type none -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_01_gm_npc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_01.par 1e-16 -ksp_type gmres -pc_type jacobi -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_01_gm_wjc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_01.par 1e-16 -ksp_type gmres -pc_type sor -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_01_gm_sor.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_01.par 1e-16 -ksp_type gmres -pc_type eisenstat -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_01_gm_eis.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_01.par 1e-16 -ksp_type gmres -pc_type ilu -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_01_gm_ilu.ppm
 
-mpirun -n 1 ./model data/par/rnd_2d_01.par
-mpirun -n 1 ./model data/par/rnd_2d_02.par
-mpirun -n 1 ./model data/par/rnd_2d_03.par
-mpirun -n 1 ./model data/par/rnd_2d_04.par
-mpirun -n 1 ./model data/par/rnd_2d_05.par
-mpirun -n 1 ./model data/par/rnd_2d_06.par
-mpirun -n 1 ./model data/par/rnd_2d_07.par
-mpirun -n 1 ./model data/par/rnd_2d_08.par
-mpirun -n 1 ./model data/par/rnd_2d_09.par
-mpirun -n 1 ./model data/par/rnd_2d_10.par
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_02.par 1e-16 -ksp_type gmres -pc_type none -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_02_gm_npc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_02.par 1e-16 -ksp_type gmres -pc_type jacobi -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_02_gm_wjc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_02.par 1e-16 -ksp_type gmres -pc_type sor -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_02_gm_sor.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_02.par 1e-16 -ksp_type gmres -pc_type eisenstat -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_02_gm_eis.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_02.par 1e-16 -ksp_type gmres -pc_type ilu -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_02_gm_ilu.ppm
+
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_03.par 1e-16 -ksp_type gmres -pc_type none -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_03_gm_npc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_03.par 1e-16 -ksp_type gmres -pc_type jacobi -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_03_gm_wjc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_03.par 1e-16 -ksp_type gmres -pc_type sor -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_03_gm_sor.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_03.par 1e-16 -ksp_type gmres -pc_type eisenstat -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_03_gm_eis.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_03.par 1e-16 -ksp_type gmres -pc_type ilu -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_03_gm_ilu.ppm
+
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_04.par 1e-16 -ksp_type gmres -pc_type none -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_04_gm_npc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_04.par 1e-16 -ksp_type gmres -pc_type jacobi -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_04_gm_wjc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_04.par 1e-16 -ksp_type gmres -pc_type sor -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_04_gm_sor.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_04.par 1e-16 -ksp_type gmres -pc_type eisenstat -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_04_gm_eis.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_04.par 1e-16 -ksp_type gmres -pc_type ilu -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_04_gm_ilu.ppm
+
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_05.par 1e-16 -ksp_type gmres -pc_type none -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_05_gm_npc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_05.par 1e-16 -ksp_type gmres -pc_type jacobi -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_05_gm_wjc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_05.par 1e-16 -ksp_type gmres -pc_type sor -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_05_gm_sor.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_05.par 1e-16 -ksp_type gmres -pc_type eisenstat -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_05_gm_eis.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_05.par 1e-16 -ksp_type gmres -pc_type ilu -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_05_gm_ilu.ppm
+
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_06.par 1e-16 -ksp_type gmres -pc_type none -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_06_gm_npc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_06.par 1e-16 -ksp_type gmres -pc_type jacobi -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_06_gm_wjc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_06.par 1e-16 -ksp_type gmres -pc_type sor -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_06_gm_sor.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_06.par 1e-16 -ksp_type gmres -pc_type eisenstat -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_06_gm_eis.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_06.par 1e-16 -ksp_type gmres -pc_type ilu -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_06_gm_ilu.ppm
+
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_07.par 1e-16 -ksp_type gmres -pc_type none -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_07_gm_npc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_07.par 1e-16 -ksp_type gmres -pc_type jacobi -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_07_gm_wjc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_07.par 1e-16 -ksp_type gmres -pc_type sor -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_07_gm_sor.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_07.par 1e-16 -ksp_type gmres -pc_type eisenstat -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_07_gm_eis.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_07.par 1e-16 -ksp_type gmres -pc_type ilu -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_07_gm_ilu.ppm
+
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_08.par 1e-16 -ksp_type gmres -pc_type none -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_08_gm_npc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_08.par 1e-16 -ksp_type gmres -pc_type jacobi -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_08_gm_wjc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_08.par 1e-16 -ksp_type gmres -pc_type sor -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_08_gm_sor.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_08.par 1e-16 -ksp_type gmres -pc_type eisenstat -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_08_gm_eis.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_08.par 1e-16 -ksp_type gmres -pc_type ilu -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_08_gm_ilu.ppm
+
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_09.par 1e-16 -ksp_type gmres -pc_type none -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_09_gm_npc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_09.par 1e-16 -ksp_type gmres -pc_type jacobi -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_09_gm_wjc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_09.par 1e-16 -ksp_type gmres -pc_type sor -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_09_gm_sor.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_09.par 1e-16 -ksp_type gmres -pc_type eisenstat -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_09_gm_eis.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_09.par 1e-16 -ksp_type gmres -pc_type ilu -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_09_gm_ilu.ppm
+
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_10.par 1e-16 -ksp_type gmres -pc_type none -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_10_gm_npc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_10.par 1e-16 -ksp_type gmres -pc_type jacobi -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_10_gm_wjc.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_10.par 1e-16 -ksp_type gmres -pc_type sor -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_10_gm_sor.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_10.par 1e-16 -ksp_type gmres -pc_type eisenstat -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_10_gm_eis.ppm
+#mpirun -n 1 ./model data/par/rnd2D/rnd_2d_10.par 1e-16 -ksp_type gmres -pc_type ilu -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd2D/rnd_2d_10_gm_ilu.ppm
+
--- quickRun3D.sh
+++ quickRun3D.sh
@@ -1,16 +1,26 @@
-mpirun -n 1 ./model data/par/tri_3d_01.par
-mpirun -n 1 ./model data/par/tri_3d_02.par
-mpirun -n 1 ./model data/par/tri_3d_03.par
-mpirun -n 1 ./model data/par/tri_3d_04.par
+mpirun -n 1 ./model data/par/tri/tri_3d_01.par 1e-16
+mpirun -n 1 ./model data/par/tri/tri_3d_02.par 1e-16
+mpirun -n 1 ./model data/par/tri/tri_3d_03.par 1e-16
+mpirun -n 1 ./model data/par/tri/tri_3d_04.par 1e-16
 
-mpirun -n 1 ./model data/par/tri_3d_add_01.par
-mpirun -n 1 ./model data/par/tri_3d_add_02.par
-mpirun -n 1 ./model data/par/tri_3d_add_03.par
-mpirun -n 1 ./model data/par/tri_3d_add_04.par
+mpirun -n 1 ./model data/par/tri/tri_3d_add_01.par 1e-16
+mpirun -n 1 ./model data/par/tri/tri_3d_add_02.par 1e-16
+mpirun -n 1 ./model data/par/tri/tri_3d_add_03.par 1e-16
+mpirun -n 1 ./model data/par/tri/tri_3d_add_04.par 1e-16
 
-mpirun -n 1 ./model data/par/lat_3d_02.par
+mpirun -n 1 ./model data/par/lat3D/lat_3d_02.par 1e-16
 
-#mpirun -n 1 ./model data/par/rnd_3d_s01_f600.par
-#mpirun -n 1 ./model data/par/rnd_3d_s01_f600_nt.par
-#mpirun -n 1 ./model data/par/rnd_3d_s01_f800.par
-#mpirun -n 1 ./model data/par/rnd_3d_s01_f800_nt.par
+#mpirun -n 1 ./model data/par/rnd3D/rnd_3d_s01_f600.par 1e-16
+#mpirun -n 1 ./model data/par/rnd3D/rnd_3d_s01_f600_nt.par 1e-16
+#mpirun -n 1 ./model data/par/rnd3D/rnd_3d_s01_f800.par 1e-16
+#mpirun -n 1 ./model data/par/rnd3D/rnd_3d_s01_f800_nt.par 1e-16
+
+#mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s01_f1000.par 1e-16 -ksp_type gmres -ksp_max_it 10 -pc_type none -ksp_atol 1e-8 -ksp_monitor_true_residual
+#mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s01_f1000.par 1e-16 -ksp_type gmres -ksp_max_it 10 -pc_type jacobi -ksp_atol 1e-8 -ksp_monitor_true_residual
+#mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s01_f1000.par 1e-16 -ksp_type gmres -ksp_max_it 10 -pc_type sor -ksp_atol 1e-8 -ksp_monitor_true_residual
+#mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s01_f1000.par 1e-16 -ksp_type gmres -ksp_max_it 10 -pc_type ilu -ksp_atol 1e-8 -ksp_monitor_true_residual
+
+#mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s01_f1000.par 1e-16 -ksp_type gmres -ksp_max_it 1000 -pc_type none -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd3D/rnd_3d_v3_s01_f1000_gm_npc.ppm
+#mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s01_f1000.par 1e-16 -ksp_type gmres -ksp_max_it 1000 -pc_type jacobi -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd3D/rnd_3d_v3_s01_f1000_gm_wjc.ppm
+#mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s01_f1000.par 1e-16 -ksp_type gmres -ksp_max_it 1000 -pc_type sor -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd3D/rnd_3d_v3_s01_f1000_gm_sor.ppm
+#mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s01_f1000.par 1e-16 -ksp_type gmres -ksp_max_it 1000 -pc_type ilu -ksp_atol 1e-8 -ksp_monitor_lg_residualnorm -draw_save_final_image data/img/rnd3D/rnd_3d_v3_s01_f1000_gm_ilu.ppm
