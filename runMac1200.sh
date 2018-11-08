035b05b4871ac287c4fd438c47220ec40d62b440
M-R-Houghton <sparky3@live.co.uk>
2018-11-08 11:18:36 +0000

    updated to more values

---
+++ runMac1200.sh
@@ -0,0 +1,21 @@
+
+
+mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s03_f1200.par 0
+
+mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s03_f1200.par 1e-12
+mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s03_f1200.par 1e-11
+mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s03_f1200.par 1e-10
+mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s03_f1200.par 1e-9
+mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s03_f1200.par 1e-8
+mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s03_f1200.par 1e-7
+mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s03_f1200.par 1e-6
+mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s03_f1200.par 1e-5
+mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s03_f1200.par 1e-4
+mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s03_f1200.par 1e-3
+mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s03_f1200.par 1e-2
+mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s03_f1200.par 1e-1
+mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s03_f1200.par 1e0
+mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s03_f1200.par 1e1
+mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s03_f1200.par 1e2
+mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s03_f1200.par 1e3
+mpirun -n 1 ./model data/par/rnd3D/rnd_3d_v3_s03_f1200.par 1e4
