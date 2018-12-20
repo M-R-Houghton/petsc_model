5f68ba7f0c33e6307cc9cc14366dd41f1098e830
M-R-Houghton <sparky3@live.co.uk>
2018-12-20 17:09:50 +0000

    added comments

--- tests/otherTests/diff_output_parser.py
+++ tests/otherTests/diff_output_parser.py
@@ -5,17 +5,24 @@ import sys
 
 if __name__ == '__main__':
 
-    # Check network_general_analysis.py has a valid number of arguments:
+    # pipe handling - diff output should be directly piped in
     if not sys.stdin.isatty():
         diff_output = sys.stdin.read()
+
+    # error handling - check for incorrect number of args
     elif len(sys.argv) != 2:
         sys.exit('Usage: diff_output_parser.py <diff_output>')
+
+    # argument handling - file should be provided
     else:
         diff_output_file = sys.argv[1]
 
+        # open provided file
         with open(diff_output_file, 'r') as fileObject:
             diff_output = fileObject.readlines()
 
+    # diff output depends on how it was read in
+    # but should be empty for success
     if diff_output == "" or diff_output == []:
         print("[PASSED]")
     else:
