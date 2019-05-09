2c9f02aae5f6e3ffc622ed471795f7b7f24c16d9
M-R-Houghton <sparky3@live.co.uk>
2019-05-09 12:35:08 +0100

    source formatting

--- src/systemAssembly.c
+++ src/systemAssembly.c
@@ -22,20 +22,20 @@ PetscErrorCode systemAssembly(Box *box_ptr, Parameters *par_ptr, Mat H, Vec b)
      * Assemble matrix (REFERENCE)
      */
     /*
-       PetscInt       i,n = 10,col[3];
-       PetscScalar    value[3];
-       ierr = PetscPrintf(PETSC_COMM_WORLD,"[STATUS] Assembling system...\n");CHKERRQ(ierr);
-       value[0] = -1.0; value[1] = 2.0; value[2] = -1.0;
-       for(i=1; i<n-1; i++) 
-       {
-       col[0] = i-1; col[1] = i; col[2] = i+1;
-       ierr   = MatSetValues(H,1,&i,3,col,value,INSERT_VALUES);CHKERRQ(ierr);
-       }
-       i    = n - 1; col[0] = n - 2; col[1] = n - 1;
-       ierr = MatSetValues(H,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
-       i    = 0; col[0] = 0; col[1] = 1; value[0] = 2.0; value[1] = -1.0;
-       ierr = MatSetValues(H,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr); // where &i is an array of i
-       */
+    PetscInt       i,n = 10,col[3];
+    PetscScalar    value[3];
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"[STATUS] Assembling system...\n");CHKERRQ(ierr);
+    value[0] = -1.0; value[1] = 2.0; value[2] = -1.0;
+    for(i=1; i<n-1; i++) 
+    {
+    col[0] = i-1; col[1] = i; col[2] = i+1;
+    ierr   = MatSetValues(H,1,&i,3,col,value,INSERT_VALUES);CHKERRQ(ierr);
+    }
+    i    = n - 1; col[0] = n - 2; col[1] = n - 1;
+    ierr = MatSetValues(H,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
+    i    = 0; col[0] = 0; col[1] = 1; value[0] = 2.0; value[1] = -1.0;
+    ierr = MatSetValues(H,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr); // where &i is an array of i
+    */
 
     ierr = MatAssemblyBegin(H,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
     ierr = MatAssemblyEnd(H,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
