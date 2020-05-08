c4cd3528b36748659717f73dad8e2145fa00185a
M-R-Houghton <sparky3@live.co.uk>
2020-05-08 19:33:20 +0100

    first BAD attempt of automating dilution network gen

--- auto_dilute_gen.sh
+++ auto_dilute_gen.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-if mpiexec -n 1 ./model data/par/lat2D/lat2d_60_s03.par> /dev/null 2>&1; then
+if mpiexec -n 1 ./model data/par/lat2D/lat2d_60_s01.par> /dev/null 2>&1; then
     echo "program succeeded"
 else
     echo "program failed"
 fi
+
+until mpiexec -n 1 ./model data/par/lat2D/lat2d_60_s01.par> /dev/null 2>&1; do
+    if [ $? -eq 0 ]; then
+        echo "Program success"
+    else
+        echo "Error was not 5"
+    fi
+    # potentially, other code follows...
+done
